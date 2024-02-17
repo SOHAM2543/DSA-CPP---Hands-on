a0b6931c9632bf41e9b29982daa54a44934cd234
Soham Vishnu Jagtap <91179991+SOHAM2543@users.noreply.github.com>
2024-02-17 20:50:00 +0530

    Create 6. vowel or not.cpp

---
+++ 6. vowel or not.cpp
@@ -0,0 +1,24 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    char character;
+    cout<<"Enter a character : ";
+    cin>>character;
+    if( character == 'a') {
+        cout<<"Character is vowel";
+    } else if(character == 'e'){
+        cout<<"Character is vowel";
+    }else if(character == 'i') {
+        cout<<"Character is vowel";
+    } else if(character == 'o'){
+        cout<<"Character is vowel";
+    } else if(character == 'u') {
+        cout<<"Character is vowel";
+    } else {
+        cout<<"Character is not vowel";
+    }
+
+    return 0;
+}
