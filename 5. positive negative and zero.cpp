bca6f4d90819712f69be3e279f45812321e1be88
Soham Vishnu Jagtap <91179991+SOHAM2543@users.noreply.github.com>
2024-02-17 20:40:08 +0530

    Create 5. positive negative and zero.cpp

---
+++ 5. positive negative and zero.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cout<<"enter a number : ";
+    cin>>a;
+    if(a > 0) {
+        cout<<"Number is positive";
+    } else {
+        if(a == 0){
+        cout<<"number is zero";
+    } else {
+        cout<<"number is negative";
+    }
+    }
+
+    return 0;
+}
