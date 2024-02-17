a05bfcbcca775284beae5c79aaea9665ee1853e2
Soham Vishnu Jagtap <91179991+SOHAM2543@users.noreply.github.com>
2024-02-17 20:15:57 +0530

    Create 1Evenodd.cpp

---
+++ 1Evenodd.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int no;
+    cout<<"enter a number: ";
+    cin>>no;
+    if(no % 2 == 0) {
+        cout<<"number is even";
+    } else {
+        cout<<"number is odd";
+    }
+
+    return 0;
+}
