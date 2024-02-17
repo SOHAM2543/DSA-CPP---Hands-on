8773c225960ec25972f14346c03b3b107a8d08fa
Soham Vishnu Jagtap <91179991+SOHAM2543@users.noreply.github.com>
2024-02-17 20:28:32 +0530

    Create 3. Compare two var.cpp

---
+++ 3. Compare two var.cpp
@@ -0,0 +1,16 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a; int b;
+    cout<<"enter two numbers : ";
+    cin>>a>>b;
+    if(a > b){
+        cout<<"a is greater";
+    } else {
+        cout<<"b is greater";
+    }
+
+    return 0;
+}
