6abcc34780c0fda0df74a48d91bad73472f33ee0
Soham Vishnu Jagtap <91179991+SOHAM2543@users.noreply.github.com>
2024-02-18 23:33:35 +0530

    Create 9. Print sentence multiple times.cpp

---
+++ 9. Print sentence multiple times.cpp
@@ -0,0 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    for(i=1;i<=10;i++) {
+        cout<<"Soham Jagtap \n";
+    }
+    
+
+    return 0;
+}
