0ff927b5ed4d97c264633b52a0ab8b18b62fd304
Soham Vishnu Jagtap <91179991+SOHAM2543@users.noreply.github.com>
2024-02-18 23:44:47 +0530

    Update 10. Even numbers.cpp

--- 10. Even numbers.cpp
+++ 10. Even numbers.cpp
@@ -5,7 +5,11 @@ int main() {
     int no,i;
     cout<<"Enter a number: ";
     cin>>no;
-    for(i = 2; i<=no; i=i+2) {
+    for(i = 2; i<=no; i++) {
+        if(i%2==0){
         cout<<i;
+        }
+        else {
+        }
     }
 }
