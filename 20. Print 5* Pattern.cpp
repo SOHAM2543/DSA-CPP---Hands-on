bbfc68ac9817a1698ef3b3b88980d1c024d1cf24
Soham Vishnu Jagtap <91179991+SOHAM2543@users.noreply.github.com>
2024-02-19 21:25:37 +0530

    Create 20. Print 5* Pattern.cpp

---
+++ 20. Print 5* Pattern.cpp
@@ -0,0 +1,16 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j;
+    for(i=1;i<=5;i++) {
+        for(j=1;j<=5;j++) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+
+    return 0;
+}
