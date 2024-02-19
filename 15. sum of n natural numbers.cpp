6d365ed3e6ad10e6d43afff4b97aa4e26cc0a636
Soham Vishnu Jagtap <91179991+SOHAM2543@users.noreply.github.com>
2024-02-19 15:03:49 +0530

    Create 15. sum of n natural numbers.cpp

---
+++ 15. sum of n natural numbers.cpp
@@ -0,0 +1,16 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum,i,n;
+    cout<<"enter number: ";
+    cin>>n;
+    for(i=1;i<n;i++) {
+        sum = sum+i;
+    }
+    cout<<"sum of natural no is :"<<sum;
+    
+
+    return 0;
+}
