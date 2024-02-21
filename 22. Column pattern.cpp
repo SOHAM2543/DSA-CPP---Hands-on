c46d067989a631238b155b62beef4488618f704e
Soham Vishnu Jagtap <91179991+SOHAM2543@users.noreply.github.com>
2024-02-21 19:38:57 +0530

    Create 22. Column pattern.cpp

---
+++ 22. Column pattern.cpp
@@ -0,0 +1,17 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j;
+    for(i=1;i<=5;i++) {
+        for(j=1;j<=5;j++) {
+            cout<<j;
+            
+        }
+        cout<<endl;
+    }
+    
+
+    return 0;
+}
