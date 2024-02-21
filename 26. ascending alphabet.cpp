c67e9cb9c9d7b4f7039e958d439d94b7757fe6e8
Soham Vishnu Jagtap <91179991+SOHAM2543@users.noreply.github.com>
2024-02-21 20:34:53 +0530

    Create 26. ascending alphabet.cpp

---
+++ 26. ascending alphabet.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    char j;
+    for(i=1;i<=5;i++) {
+        for(j='a';j<='e';j++) {
+            cout<<j<<"";
+            
+        }
+        cout<<endl;
+    }
+    
+
+    return 0;
+}
