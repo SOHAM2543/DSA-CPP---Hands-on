1a9fa68f3fcb3ec7ad3b29dad20b4bec283fa970
Soham Vishnu Jagtap <91179991+SOHAM2543@users.noreply.github.com>
2024-02-22 21:15:19 +0530

    Create 28. Ascending number.cpp

---
+++ 28. Ascending number.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j;
+   
+    for(i=1;i<=5;i++) {
+        for(j=1;j<=i;j++) {
+            cout<<j<<"";
+            
+        }
+        cout<<endl;
+    }
+    
+
+    return 0;
+}
