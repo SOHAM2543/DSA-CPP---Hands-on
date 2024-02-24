eec564da54041c7721c2a9ba138bffb3939d9515
Soham Vishnu Jagtap <91179991+SOHAM2543@users.noreply.github.com>
2024-02-24 20:46:54 +0530

    Create 37. Pyramid 123321.cpp

---
+++ 37. Pyramid 123321.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j,n;
+    cout<<"enter a number:";
+    cin>>n;
+    for(int i = 1; i <= n; i++) {
+        for(j = 1; j<= n - i; j++) 
+            cout<<" ";
+        
+        for(j = 1; j<= i;j++) 
+            cout<<j;
+        for(j = i-1; j>= 1; j--)
+            cout<<j<<"";
+        
+        cout<<endl;
+    }
+   
+
+    return 0;
+}
