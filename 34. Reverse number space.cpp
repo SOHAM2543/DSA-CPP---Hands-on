bbca2cbfa04ef33ec435140ab873d28d1c12a50f
Soham Vishnu Jagtap <91179991+SOHAM2543@users.noreply.github.com>
2024-02-24 19:43:18 +0530

    Create 34. Reverse number space.cpp

---
+++ 34. Reverse number space.cpp
@@ -0,0 +1,21 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j,n;
+    cout<<"enter a number:";
+    cin>>n;
+    for(int i = 1; i <= n; i++) {
+        for(j = 1; j<= n - i; j++) {
+            cout<<" ";
+        }
+        for(j = 1; j<=i; j++) {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+   
+
+    return 0;
+}
