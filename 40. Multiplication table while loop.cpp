e5e2e318b87ac9026336a56edd3256ab0ba8e4b7
Soham Vishnu Jagtap <91179991+SOHAM2543@users.noreply.github.com>
2024-02-25 11:49:01 +0530

    Create 40. Multiplication table while loop.cpp

---
+++ 40. Multiplication table while loop.cpp
@@ -0,0 +1,17 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,n;
+    cout<<"Enter Number:";
+    cin>>n;
+    while(i<=10){
+        cout<<n * i<<endl;
+        i++;
+    }
+    
+    
+
+    return 0;
+}
